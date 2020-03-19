c8b03aeffd25dad22a9697380290e63787a5efe6
BranceLee <475815059@qq.com>
2020-03-19 19:12:37 +0800

    rm Product code from login page

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -2,27 +2,20 @@
   <div>
     <component :is="currentComponent" />
   </div>
-  <!-- <button @click="getProduct">Select</button> -->
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import LoginForm from './components/Form.vue';
-// import ManageUserForm from './components/ManageUserForm.vue';
 
 @Component({
   name: 'Login',
   components: {
     LoginForm
-    // ManageUserForm
   }
 })
 export default class extends Vue {
   private currentComponent = 'LoginForm';
-
-  // private getProduct(){
-
-  // }
 }
 </script>
 
